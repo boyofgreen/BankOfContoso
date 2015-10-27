a8d794d7f71e9b8ac798f601e71e06d6599bc9c7
Jeff Burtoft <boyofgreen@gmail.com>
2015-10-27 09:51:22 -0500

    show use of windows features

--- src/js/windowsapp.js
+++ src/js/windowsapp.js
@@ -10,7 +10,7 @@ if(window.Windows){
 	console.log('this printed from the file@')
 
 
-
+   setTimeout(function(){ document.getElementById('onlyWindows').style.display = '';},1000);
 
 function createToast(title, message, imgUrl, imgAlt, tag, lang) {
 	    
@@ -99,6 +99,39 @@ function notify () {
 }
 
 setTimeout(notify, 5000);
+
+Windows.UI.ViewManagement.ApplicationView.getForCurrentView().titleBar.backgroundColor = {a:255, r:48, g:66, b:106};
+Windows.UI.ViewManagement.ApplicationView.getForCurrentView().titleBar.buttonBackgroundColor = { a: 255, r: 48, g: 66, b: 106 };
+Windows.UI.ViewManagement.ApplicationView.getForCurrentView().titleBar.buttonForegroundColor = Windows.UI.Colors.white;
+Windows.UI.ViewManagement.ApplicationView.getForCurrentView().titleBar.foregroundColor = Windows.UI.Colors.white;
+
+function updateTile(message, imgUrl, imgAlt) {
+    // Namespace: Windows.UI.Notifications
+    if (typeof Windows !== 'undefined'&&
+            typeof Windows.UI !== 'undefined' &&
+            typeof Windows.UI.Notifications !== 'undefined') {
+
+        var notifications = Windows.UI.Notifications,
+        tile = notifications.TileTemplateType.tileSquare150x150PeekImageAndText01,
+        tileContent = notifications.TileUpdateManager.getTemplateContent(tile),
+        tileText = tileContent.getElementsByTagName('text'),
+        tileImage = tileContent.getElementsByTagName('image');
+
+        tileText[0].appendChild(tileContent.createTextNode(message || 'Demo Message'));
+        tileImage[0].setAttribute('src', imgUrl || 'https://unsplash.it/150/150/?random');
+        tileImage[0].setAttribute('alt', imgAlt || 'Random demo image');
+
+        var tileNotification = new notifications.TileNotification(tileContent);
+        var currentTime = new Date();
+        tileNotification.expirationTime = new Date(currentTime.getTime() + 600 * 1000);
+        notifications.TileUpdateManager.createTileUpdaterForApplication().update(tileNotification);
+    } else {
+        //Alternative behavior
+    }
+
+}
+ updateTile();
+
 	
 }
 
